8912d59b6f804e74822f8eb17d83d313194b883b
MartineIversen <43151774+MartineIversen@users.noreply.github.com>
2019-12-11 14:34:37 +0100

    working map is more awesome
    300$

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-
-  });
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.pushpin');
-    var instances = M.Pushpin.init(elems, {
-        top: 767,
-        offset: 200
-    });
-  });
-
 // -------> Google maps
 
 
