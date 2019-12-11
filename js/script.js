d582f725b279286d1c3c477209db4dc6a5f1688f
MartineIversen <43151774+MartineIversen@users.noreply.github.com>
2019-12-11 13:58:35 +0100

    awesome map
    its 300$, thanks

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
-      
+
   });
  document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.pushpin');
@@ -11,3 +11,15 @@
     });
   });
 
+// -------> Google maps
+
+
+function initMap() {
+  // The location of Aarhus
+  var aarhus = {lat: 56.146301, lng: 10.193522};
+  // The map, centered at Aarhus
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 20, center: aarhus});
+  // The marker, positioned in Aarhus
+  var marker = new google.maps.Marker({position: aarhus, map: map});
+}
