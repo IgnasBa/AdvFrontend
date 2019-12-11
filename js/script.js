6d86dbc29715e38db8772e0e89cf93ad11202f5f
Eduard <edward.roman8@gmail.com>
2019-12-11 13:40:13 +0100

    Made Gallery for Phone

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
-      
-  });
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.pushpin');
-    var instances = M.Pushpin.init(elems, {
-        top: 767,
-        offset: 200
-    });
+
   });
 
+  // Swiper initiliaze
+
+  var swiper = new Swiper('.swiper-container', {
+    observer: true,
+    observeParents: true,
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: false,
+      slideShadows: false,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
