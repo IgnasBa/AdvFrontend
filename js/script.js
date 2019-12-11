9001c239a0d738ce1bfda9cd5e1a9162f91e9ee0
IgnasBa <43188272+IgnasBa@users.noreply.github.com>
2019-12-11 11:19:02 +0100

    push[in

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
+      
   });
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.pushpin');
+    var instances = M.Pushpin.init(elems, {
+        top: 767,
+        offset: 200
+    });
+  });
+
